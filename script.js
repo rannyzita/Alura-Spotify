154c4d82cbb3dd31530b45dee7a7beb0d991d8f5
rannyzita <fs.vitoria.soares@gmail.com>
2025-02-14 17:08:47 -0300

    add: fix position card part 2

--- script.js
+++ script.js
@@ -6,20 +6,40 @@ function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result, searchTerm))
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden')
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    resultPlaylist.classList.add('hidden');
+    resultArtist.innerHTML = ''; // Limpa os resultados antigos
+
+    if (result.length === 0) {
+        resultArtist.classList.remove('active'); // Esconde se não houver resultados
+        return;
+    }
 
     result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
+        const artistCard = document.createElement('div');
+        artistCard.classList.add('artist-card');
+
+        artistCard.innerHTML = `
+            <div class="card-img">
+                <img class="artist-img" src="${element.urlImg}" alt="${element.name}" />
+                <div class="play">
+                    <span class="fa fa-solid fa-play"></span>
+                </div>
+            </div>
+            <div class="card-text">
+                <a title="${element.name}" class="vst" href=""></a>
+                <span class="artist-name">${element.name}</span>
+                <span class="artist-categorie">Artista</span>
+            </div>
+        `;
+
+        resultArtist.appendChild(artistCard);
     });
 
-    resultArtist.classList.remove('hidden');
+    resultArtist.classList.add('active'); // Exibe os resultados apenas se houver artistas
 }
 
 document.addEventListener('input', function () {
@@ -30,6 +50,6 @@ document.addEventListener('input', function () {
         resultArtist.innerHTML = ''; // Limpa os resultados ao apagar a pesquisa
         return;
     }
-    
+
     requestApi(searchTerm);
 });
